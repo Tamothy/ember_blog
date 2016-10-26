f599c16c9a8713d35ffbaa22340bbf8ab3459241
April Peng and Tam Nguyen <april+tam@>
2016-10-26 13:26:40 -0700

    add font awesome ability

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     toggleSidebar() {
-      $('.ui.sidebar').sidebar('toggle');
+      Ember.$('.ui.sidebar').sidebar('toggle');
     }
   }
 });
